69d3c7537198908bdf4c545ad4f9d8977433e1bd
itsRavindra001 <163872904+itsRavindra001@users.noreply.github.com>
2025-08-16 00:02:15 +0530

    Update vite.config.ts

--- localkart/vite.config.ts
+++ localkart/vite.config.ts
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000', // Backend server
+//         //  target: 'http://localcart-ujab.onrender.com/', // Backend server
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Backend server
-        //  target: 'http://localcart-ujab.onrender.com/', // Backend server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+  plugins: [react()],
+  base: "/",
+});
+
